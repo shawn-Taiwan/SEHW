17318f31f21a9a86648d505e534b9d41cc6633de
shawn-Taiwan <shawn82tw@gmail.com>
2020-12-29 01:53:40 +0800

    add main

--- src/letterGrade.java
+++ src/letterGrade.java
@@ -1,25 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class letterGrade {
-    public  static char letterGrade(int score){
-        char grade;
-        if(score<0 || score >100)
-            grade = 'X';
-        else if(score>=90 && score <= 100)
-            grade = 'A';
-        else if(score>=80 && score < 90)
-            grade = 'B';
-        else if(score>=70 && score < 80)
-            grade = 'C';
-        else if(score>=60 && score < 70)
-            grade = 'D';
-        else
-            grade = 'F';
-        return grade;
-    }
-
     @Test
     public void letterGradetest() {
         char level = letterGrade(-1);
@@ -81,4 +68,38 @@ public class letterGrade {
         level = letterGrade(100);
         assertEquals('A',level);
     }
+
+    public  static char letterGrade(int score){
+        char grade;
+        if(score<0 || score >100)
+            grade = 'X';
+        else if(score>=90 && score <= 100)
+            grade = 'A';
+        else if(score>=80 && score < 90)
+            grade = 'B';
+        else if(score>=70 && score < 80)
+            grade = 'C';
+        else if(score>=60 && score < 70)
+            grade = 'D';
+        else
+            grade = 'F';
+        return grade;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = letterGrade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
